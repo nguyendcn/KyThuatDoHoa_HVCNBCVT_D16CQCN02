eaa1772c811318cb51ce98f1bff505abce67f3e4
Mr. Nguyên <superntseal@gmail.com>
2019-05-11 23:16:55 +0700

    Update change from interface Shapes2DObject.

--- KyThuatDoHoa_Nhom9/Construct/_2DObject/Line.cs
+++ KyThuatDoHoa_Nhom9/Construct/_2DObject/Line.cs
@@ -80,62 +80,24 @@ namespace KyThuatDoHoa_Nhom9.Construct._2DObject
             this.ColorOfLine = (color);
         }
 
-
-        //public bool impact(Point p)
-        //{
-        //    int AB = (int)Math.sqrt(Math.pow((A.x - B.x), 2) + Math.pow(A.y - B.y, 2));
-        //    int AP = (int)Math.sqrt(Math.pow((A.x - p.x), 2) + Math.pow(A.y - p.y, 2));
-        //    int PB = (int)Math.sqrt(Math.pow((p.x - B.x), 2) + Math.pow(p.y - B.y, 2));
-        //    return (((AP + PB) - AB) < size);
-        //}
-
-
-        //public void move(Point start, Point end)
-        //{
-        //    int dx = end.x - start.x;
-        //    int dy = end.y - start.y;
-        //    this.setA(new Point(this.getA().x + dx, this.getA().y + dy));
-        //    this.setB(new Point(this.getB().x + dx, this.getB().y + dy));
-        //}
-
-
-        public void Rotate(Point a, Direction b)
+        public void Rotate(Point p, double alpha)
         {
-            //float a = Display.angleBetweenTwoLines(start, end);
-            //Point p1 = new Point(Display.rotateAround(this.getA(), a));
-            //Point p2 = new Point(Display.rotateAround(this.getB(), a));
-            //setA(p1);
-            //setB(p2);
-            //this.setAngle(this.getAngle() + a);
+            throw new NotImplementedException();
         }
 
-
-        public void Scale(Point start, Point end)
+        public void Shifting(Point pDest)
         {
-            //double AP = Math.sqrt(Math.pow((this.getA().x - start.x), 2) + Math.pow(this.getA().y - start.y, 2));
-            //double PB = Math.sqrt(Math.pow((start.x - this.getB().x), 2) + Math.pow(start.y - this.getB().y, 2));
-            //if (AP < PB)
-            //{
-            //    this.setA(this.getB());
-            //    this.setB(end);
-            //}
-            //else
-            //{
-            //    this.setA(this.getA());
-            //    this.setB(end);
-            //}
+            throw new NotImplementedException();
         }
 
-        public void Shifting(Direction dir)
+        public void Symmetry(Point orgin, SymmetryMode mode)
         {
-
+            throw new NotImplementedException();
         }
 
-        public void Symmetry()
+        public void Scale(SizeF scaleSize)
         {
-
+            throw new NotImplementedException();
         }
-
-        
     }
 }
