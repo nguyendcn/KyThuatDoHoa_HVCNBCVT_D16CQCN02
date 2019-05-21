73e6241451c5be82a5fcd0b7075a776c483cb353
Mr. Nguyên <superntseal@gmail.com>
2019-05-21 22:00:32 +0700

    Created class Timepiece.cs

---
+++ KyThuatDoHoa_Nhom9/Construct/_2DObject/Timepiece.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KyThuatDoHoa_Nhom9.Construct.DefineType;
+
+namespace KyThuatDoHoa_Nhom9.Construct._2DObject
+{
+    public class Timepiece : Shapes2DObject, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void Draw(Graphics g)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Init(Point start, Point end, Size sizeOfLine, Color color)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Rotate(Point p, double alpha)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scale(SizeF scaleSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Shifting(Point pDest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Symmetry(Point orgin, SymmetryMode mode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
